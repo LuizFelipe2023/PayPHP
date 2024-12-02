e9ef93a91280da63237b5e489adb1dd769eab1f1
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2024-12-02 16:59:08 -0400

    feita a classe Usuario.php

--- App/models/Usuario.php
+++ App/models/Usuario.php
@@ -1,10 +1,71 @@
 <?php
 
-class Usuario{
+namespace models;
 
-      public $id;
-      public $name;
-      public $address;
-      private $email;
-      private $password;
+class Usuario
+{
+
+    public $id;
+    public $name;
+    public $phone;
+    private $address;
+    private $email;
+    private $password;
+
+
+    public function __construct($name, $address, $phone, $email, $password)
+    {
+        $this->name = $name;
+        $this->address = $address;
+        $this->phone = $phone;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
 }
